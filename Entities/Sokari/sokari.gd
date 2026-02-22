506807db9df160a18da2511aec883265d41756bb
SamirWattar <87576341+samirwattar@users.noreply.github.com>
2026-02-22 19:56:30 +0100

    sokari

---
+++ Entities/Sokari/sokari.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+@export var max_health: float = 100.0
+@export var speed: float = 100.0
+
+@onready var anim = $AnimatedSprite2D
+
+var health: float = max_health
+var follow: PathFollow2D
+
+func _ready():
+	health = max_health
+	follow = get_parent()
+
+func _process(delta):
+	var prev_pos = follow.position
+	follow.progress += speed * delta
+	
+	if follow.progress_ratio >= 1.0:
+		follow.queue_free()
+		return
+	
+	var movement = follow.position - prev_pos
+	
+	if abs(movement.x) > abs(movement.y):
+		anim.play("walk_left")
+	else:
+		anim.play("walk_normal")
+
+func take_damage(amount: float):
+	health -= amount
+	if health <= 0:
+		die()
+
+func die():
+	follow.queue_free()
