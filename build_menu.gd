635191bf2cb08991d30564e048d945920d1f6c86
msubhikhawatmi-jpg <m.subhi.khawatmi@gmail.com>
2026-02-22 23:13:50 +0100

    erfgbd

--- build_menu.gd
+++ build_menu.gd
@@ -4,6 +4,7 @@ signal tower_selected(tower_scene)
 
 @export var archer_scene: PackedScene
 @export var mage_scene: PackedScene
+@export var canon_scene: PackedScene
 
 func _ready():
 	visible = false
@@ -17,5 +18,9 @@ func _on_archer_button_pressed():
 	visible = false
 
 func _on_mage_button_pressed():
+	tower_selected.emit(mage_scene)
+	visible = false
+	
+func _on_canon_button_pressed():
 	tower_selected.emit(mage_scene)
 	visible = false
